740e8c677180aa4e96297aa7c2a7221a9597932b
yogeshrepo <118822006+yogeshrepo@users.noreply.github.com>
2023-06-12 12:02:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const person = {
   name: "John Doe",
   age: 80,
 };
-
+const team = players;
+const team1 = players;
+document.write(players, team1);
+const cap1=person;
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
